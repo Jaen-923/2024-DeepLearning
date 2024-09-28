07765e6cec5514d39e87ab62ab81a2c21483cbd2
Jaen-923 <113357550+Jaen-923@users.noreply.github.com>
2024-09-28 17:34:39 +0900

    3주차 과제

---
+++ hw1.py
@@ -0,0 +1,14 @@
+# 연산자 오버로딩 예시
+print(12+37)
+print('python' + ' is exciting.')
+
+# 라이브러리 불러오기
+import random
+
+# 정수 난수 생성
+a = random.randint(10, 20)
+b = random.randint(10, 20)
+
+# 덧셈을 하고 결과 출력
+c = a+b
+print(a, b, c)
